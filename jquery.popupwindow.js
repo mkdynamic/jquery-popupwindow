0c1fc684a5666f69ad6aa1ea37c664f6f39a8114
Joe Lencioni <joe.lencioni@causes.com>
2014-03-01 00:51:08 -0800

    Improve code formatting
    This is an entirely cosmetic change that converts the 4 spaces per tab
    to 2 spaces per tab, wraps some long lines at 80 characters, and aligns
    some values to improve readability.

--- jquery.popupwindow.js
+++ jquery.popupwindow.js
@@ -4,65 +4,66 @@
 * Requires: jQuery v1.3.2
 */
 (function($) {
-    var defaults = {
-        height: 500,
-        width: 500,
-        toolbar: false,
-        scrollbars: false, // os x always adds scrollbars
-        status: false,
-        resizable: false,
-        left: 0,
-        top: 0,
-        center: true,
-        createNew: true,
-        name: null,
-        location: false,
-        menubar: false,
-        onUnload: null
-    };
-        
-    $.popupWindow = function(url, opts) {
-        var options = $.extend({}, defaults, opts);
+  var defaults = {
+    height:      500,
+    width:       500,
+    toolbar:     false,
+    scrollbars:  false, // os x always adds scrollbars
+    status:      false,
+    resizable:   false,
+    left:        0,
+    top:         0,
+    center:      true,
+    createNew:   true,
+    name:        null,
+    location:    false,
+    menubar:     false,
+    onUnload:    null
+  };
 
-        // center the window
-        if (options.center) {
-            // 50px is a rough estimate for the height of the chrome above the document area
-            options.top = ((screen.height - options.height) / 2) - 50;
-            options.left = (screen.width - options.width) / 2;
+  $.popupWindow = function(url, opts) {
+    var options = $.extend({}, defaults, opts);
+
+    // center the window
+    if (options.center) {
+      // 50px is a rough estimate for the height of the chrome above the
+      // document area
+      options.top = ((screen.height - options.height) / 2) - 50;
+      options.left = (screen.width - options.width) / 2;
+    }
+
+    // params
+    var params = [];
+    params.push('location=' + (options.location ? 'yes' : 'no'));
+    params.push('menubar=' + (options.menubar ? 'yes' : 'no'));
+    params.push('toolbar=' + (options.toolbar ? 'yes' : 'no'));
+    params.push('scrollbars=' + (options.scrollbars ? 'yes' : 'no'));
+    params.push('status=' + (options.status ? 'yes' : 'no'));
+    params.push('resizable=' + (options.resizable ? 'yes' : 'no'));
+    params.push('height=' + options.height);
+    params.push('width=' + options.width);
+    params.push('left=' + options.left);
+    params.push('top=' + options.top);
+
+    // open window
+    var random = new Date().getTime();
+    var name = options.name || (options.createNew ? 'popup_window_' + random : 'popup_window');
+    var win = window.open(url, name, params.join(','));
+
+    // unload handler
+    if (options.onUnload && typeof options.onUnload === 'function') {
+      var unloadInterval = setInterval(function() {
+        if (!win || win.closed) {
+          clearInterval(unloadInterval);
+          options.onUnload();
         }
+      }, 50);
+    }
 
-        // params
-        var params = [];
-        params.push('location=' + (options.location ? 'yes' : 'no'));
-        params.push('menubar=' + (options.menubar ? 'yes' : 'no'));
-        params.push('toolbar=' + (options.toolbar ? 'yes' : 'no'));
-        params.push('scrollbars=' + (options.scrollbars ? 'yes' : 'no'));
-        params.push('status=' + (options.status ? 'yes' : 'no'));
-        params.push('resizable=' + (options.resizable ? 'yes' : 'no'));
-        params.push('height=' + options.height);
-        params.push('width=' + options.width);
-        params.push('left=' + options.left);
-        params.push('top=' + options.top);
+    // focus window
+    if (win && win.focus) win.focus();
 
-        // open window
-        var random = new Date().getTime();
-        var name = options.name || (options.createNew ? 'popup_window_' + random : 'popup_window');
-        var win = window.open(url, name, params.join(','));
-
-        // unload handler
-        if (options.onUnload && typeof options.onUnload === 'function') {
-            var unloadInterval = setInterval(function() {
-                if (!win || win.closed) {
-                    clearInterval(unloadInterval);
-                    options.onUnload();
-                }
-            }, 50);
-        }
-
-        // focus window
-        if (win && win.focus) win.focus();
-        
-        // return handle to window
-        return win;
-    };
+    // return handle to window
+    return win;
+  };
 })(jQuery);
